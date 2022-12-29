63e655ce0596d7a2bfd5e95ee3f4e1f6c33cd914
Sargis Abrahamyan <118532124+Sargis-Abrahamyan@users.noreply.github.com>
2022-12-29 17:59:18 +0400

    Update MenuLink.jsx

--- src/Components/layout/sideBar/menuLink/MenuLink.jsx
+++ src/Components/layout/sideBar/menuLink/MenuLink.jsx
@@ -13,7 +13,9 @@ const MenuLink = () => {
             {
                 routeData.map((route) => {
                     return (
-                        <li key={route.id} className={`${styles.li_navlink} ${pathname === route.path ? styles.active : undefined}`} style={{ borderRadius: openMenu ? "6px" : "41px" }}>
+                        <li key={route.id}
+                            className={`${styles.li_navlink} 
+                                        ${pathname === route.path ? styles.active : undefined}`} style={{ borderRadius: openMenu ? "6px" : "41px" }}>
                             <Link to={`${route.path}`} className={styles.link_url} onClick={() => setOpenMenu(false)}>
                                 <img src={route.icon} alt="navIcon" />
                                 {openMenu && <span className={styles.linkurl_name}>{route.name}</span>}
